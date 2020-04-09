5d564359bc45101eb551f6cdbe40f9b6f9d073e3
kriskirimi <60326060+kriskirimi@users.noreply.github.com>
2020-04-09 11:20:43 +0300

    Add files via upload

--- Kenya External and Domestic Debt 1999-2018.py
+++ Kenya External and Domestic Debt 1999-2018.py
@@ -8,7 +8,6 @@ Created on Mon Feb 24 23:42:42 2020
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
-import matplotlib
 import matplotlib.ticker as mtick
 from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
@@ -26,12 +25,10 @@ df.head(10)
 df.drop(['Year', 'Month'], axis=1, inplace=True)
 df = df.round({'Domestic Debt':0, 'External Debt':0, 'Total Debt':0})
 df.head(4)
-# yourdate = '2003-01-01'
-# print((df['Dates'] == yourdate).any())
-fig, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(10,18))
-plt.style.use('bmh')
+plt.style.use('seaborn-poster')
 
 #PLOT NO1-Kenya's Domestic and External Debt, September 1999-March 2018
+fig, ax1 = plt.subplots(figsize=(8, 8))
 line1 = ax1.plot(df['Dates'], df['Domestic Debt'], label='Domestic Debt')
 line2 = ax1.plot(df['Dates'], df['External Debt'], label='External Debt')
 date_formatter_1 = mdates.DateFormatter('%Y')
@@ -47,7 +44,8 @@ ax1.yaxis.set_major_formatter(mtick.FuncFormatter(new_tick_value))
 ax1.set_title('Kenya Domestic and External Debt, (September 1999 - March 2018)')
 ax1.legend(loc='best')
 
-#PLOT NO2-Kenya's 2nd President, President Mwai Kibaki Tenure Domestic and External Debt Analysis 2nd Term (January 2003-April 2013)
+##PLOT NO2-Kenya's 2nd President, President Mwai Kibaki Tenure Domestic and External Debt Analysis 2nd Term (January 2003-April 2013)
+fig, ax2 = plt.subplots(figsize=(10, 10))
 ax2.plot(df['Dates'], df['Domestic Debt'], label='Domestic Debt')
 ax2.plot(df['Dates'], df['External Debt'], label='External Debt')
 ax2.set(xlabel='Dates', ylabel='Amount in $', xlim = ['2003-01-01', '2013-04-01'])
@@ -58,7 +56,10 @@ ax2.yaxis.set_major_formatter(mtick.FuncFormatter(new_tick_value))
 ax2.xaxis.set_major_formatter(date_formatter_2)
 ax2.set_title('President Mwai Kibaki Tenure Domestic and External Debt, (Jan 2003-April 2013)')
 ax2.legend(loc='best')
+plt.show()
+
 #PLOT NO3-Kenya's 3rd President, President Uhuru Kenyatta Tenure Domestic and External Debt Analysis (January 2003-January 2008)
+fig, ax3 = plt.subplots(figsize=(10, 10))
 ax3.plot(df['Dates'], df['Domestic Debt'], label='Domestic Debt')
 ax3.plot(df['Dates'], df['External Debt'], label='External Debt')
 ax3.set(xlabel='Dates', ylabel='Amount in $', xlim = ['2013-05-01', '2018-03-01'])
@@ -69,5 +70,4 @@ ax3.xaxis.set_major_formatter(date_formatter_3)
 ax3.yaxis.set_major_formatter(mtick.FuncFormatter(new_tick_value))
 ax3.set_title('President Uhuru Kenyatta Tenure Domestic and External Debt (January 2013-March 2018)')
 ax3.legend(loc='best')
-plt.tight_layout()
 plt.show()
